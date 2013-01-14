81a0325258f5e8966514eac7acb4568423eae263
crucialfelix <crucialfelix@gmail.com>
2013-01-14 22:14:03 +0100

    switching main to index.js from supercollider.js

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+// exports.scsynth = require('./lib/server/scsynth');
+// exports.sclang = require('./lib/server/sclang');
+
+exports.scapi = require('./lib/server/scapi-server');
+// exports.scapi = require('./lib/browser/scapi');
