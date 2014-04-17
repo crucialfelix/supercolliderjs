ef8179b8818b9fc64c9ef7e820aade34eb1c717e
Chris Sattinger <crucialfelix@gmail.com>
2014-04-17 21:50:06 +0200

    jshint

--- bin/scapi-server.js
+++ bin/scapi-server.js
@@ -26,7 +26,7 @@
 */
 
 
-var scapi = require("../lib/server/scapi-server.js");
+var scapi = require('../lib/server/scapi-server.js');
 
 /**
   * Optional arguments:
@@ -38,4 +38,3 @@ var scapi = require("../lib/server/scapi-server.js");
 */
 
 scapi.listen();
-
