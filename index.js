6a09134b866267d145357df4ee1946edc4c50394
Chris Sattinger <crucialfelix@gmail.com>
2014-04-18 16:38:40 +0200

    add parse-options: command line options parser for all scripts

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 exports.sclang = require('./lib/server/sclang');
 exports.scsynth = require('./lib/server/scsynth');
 exports.scapi = require('./lib/nodejs/scapi');
+exports.options = require('./lib/nodejs/parse-options');
