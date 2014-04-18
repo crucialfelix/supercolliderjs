a5b176240f704110b03c56b8bede6f33f17a8bcc
Chris Sattinger <crucialfelix@gmail.com>
2014-04-18 14:33:03 +0200

    sclang.js : add optional logging to console with colors

--- examples/run-sclang.js
+++ examples/run-sclang.js
@@ -1,4 +1,10 @@
 
+/**
+ *
+ * By default sclang.js echoes everything to the console with colors.
+ *
+ */
+
 var SCLang = require('../lib/nodejs/sclang.js');
 
 var dir = '/Users/crucial/code/supercollider/build/install/SuperCollider/SuperCollider-3-7.app/Contents/Resources/';
@@ -6,18 +12,9 @@ var dir = '/Users/crucial/code/supercollider/build/install/SuperCollider/SuperCo
 var sclang = new SCLang({'cwd': dir});
 sclang.boot();
 
-sclang.on('stdout', function(d) {
-	console.log('STDOUT:' + d);
-});
-
-sclang.on('stderr', function(d) {
-	console.log('STDERR:' + d);
-});
-
 // wait for it to compile
-console.log('Waiting 5 seconds till sc compiles');
+console.log('Waiting 5 seconds till sc compiles...');
 setTimeout(function() {
-	console.log('writing to stdin: "1 + 1"');
-	sclang.write("1 + 1");
+  sclang.write("1 + 1");
 }, 5000);
 
