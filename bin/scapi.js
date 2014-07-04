b5d6aa71d878d0700544156323a152c5b0ab9f2b
Chris Sattinger <crucialfelix@gmail.com>
2014-07-04 18:48:35 +0200

    docs

--- bin/scapi.js
+++ bin/scapi.js
@@ -1,28 +1,46 @@
 #!/usr/bin/env node
 
 /*
-	Runs a webserver and web-socket server for communicating from a browser webpage to a running SuperCollider (sclang) application.
+	Runs a websockets <-> OSC bridge for communicating
+	from a browser through to SuperCollider
 
-	Browser
+	Usage:
 
-	/fiddle.html
-		A page with an interface for exploring the SC API and sending/receiving responses.
+		Start SuperCollider
+		Install the API quark ( > 2.0 )
+		Activate the OSC responders in supercollider:
+			API.mountDuplexOSC
+
+
+		Start this server:
+
+		// serves a default page where you can view registered API endpoints
+		// and make api calls, get resposnes
+		// A page with an interface for exploring the SC API and sending/receiving responses.
+		scapi
+
+		// Starts a webserver with the specified path as web root.
+		// eg: current directory
+		scapi ./
+
+
+	These special URLs are also served:
+
 	/static/js/scapi.js
 		The browser side javascript library used for communciating with this server
+
 	/socket.io
 		The javascript for socket.io (for websocket support)
 
+	If you have installed this package globally then 'scapi' is on your path.
 
-    Start SuperCollider
-    Install the API quark ( > 2.0 )
-    Activate the OSC responders in supercollider:
-      API.mountDuplexOSC
+	If you have cloned this then call the file directly using node:
 
-    Start this server:
-        node bin/scapi-server.js
+      node bin/scapi-server.js
 
-    Navigate browser to
-      http://localhost:4040/fiddle.html
+  Navigate browser to:
+
+      http://localhost:4040/
 
 */
 
