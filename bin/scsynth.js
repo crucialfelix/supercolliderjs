4cc82216df4e6781ee1e9dc070bc0455d653c21b
Chris Sattinger <crucialfelix@gmail.com>
2014-08-03 16:36:13 +0200

    bin/scsynth: convert to new options system

--- bin/scsynth.js
+++ bin/scsynth.js
@@ -1,11 +1,64 @@
 #!/usr/bin/env node
 
+var help = [
+  "Run scsynth (the supercollider synthesis server) using the configuration defined in the nearest .supercollider.yaml searching up from the current working directory.",
+  "",
+  "",
+  "Examples:",
+  "",
+  "supercollider-server",
+  "supercollider-server --config=/path/to/a/custom/config.yaml",
+  "supercollider --scsynth=/path/to/scsynth",
+  ""
+];
+
 var
-    path = require('path'),
-    lib = path.join(__dirname, '../lib/nodejs/'),
+    join = require('path').join,
+    pkg = require(join(__dirname, '../package.json')),
+    lib = join(__dirname, '../lib/nodejs/'),
+    program = require('commander'),
+    resolveOptions = require('../lib/nodejs/resolveOptions'),
     Server = require(lib + 'scsynth'),
-    options = require(lib + 'parse-options');
+    options = {};
 
-var s = new Server(options());
 
-s.boot();
+function truthy(input) {
+  return (input + '') !== 'false';
+}
+
+program.version(pkg.version)
+  .option('--config <path>', 'Configuration file eg. .supercollider.yaml')
+  .option('--scsynth <path>', 'Path to scsynth executable')
+  .option('--serverPort <port>', 'UDP port for the server to listen on')
+  .option('-v, --verbose', 'Post debugging messages (default: false)', truthy, false);
+
+program.on('--help', function(){
+  help.forEach(function(line) {
+    console.info("    " + line);
+  });
+});
+
+program.parse(process.argv);
+
+['config', 'scsynth', 'serverPort', 'verbose'].forEach(function(k) {
+  if(k in program) {
+    options[k] = program[k];
+  }
+});
+
+resolveOptions(options.config, options).then(function(options) {
+
+  var s = new Server(options);
+
+  s.boot();
+
+  s.on('exit', function() {
+    console.warn('scsynth exited');
+    console.info(options);
+    process.exit(1);
+  });
+
+}, function(err) {
+  console.error(err);
+  process.exit(1);
+});
