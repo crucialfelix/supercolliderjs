75d2d1c6b76072159a7c6839ca7bbd45db5d7f6e
Chris Sattinger <crucialfelix@gmail.com>
2015-05-14 19:10:03 +0200

    add tests

--- test/sclang.spec.js
+++ test/sclang.spec.js
@@ -2,6 +2,8 @@
 var supercolliderjs = require('../index');
 var SCLang = supercolliderjs.sclang;
 var should = require('should');
+var _ = require('underscore');
+var path = require('path');
 
 describe('sclang', function() {
 
@@ -10,6 +12,45 @@ describe('sclang', function() {
       var sclang = new SCLang({});
       should.exist(sclang);
     });
+  });
+
+  describe('sclangConfigOptions', function() {
+
+    it('should include sc-classes if errorsAsJSON is true', function() {
+      var sclang = new SCLang({});
+      var opts = sclang.sclangConfigOptions({errorsAsJSON: true});
+      opts.includePaths.length.should.be.exactly(1);
+      var isIn = _.some(opts.includePaths, function(p) {
+        return p.match(/sc\-classes/);
+      });
+      isIn.should.be.true;
+    });
+
+    it('should read a supplied sclang_conf', function() {
+      var sclang = new SCLang({});
+      var opts = sclang.sclangConfigOptions({
+        sclang_conf: path.join(__dirname, 'sclang_test_conf.yaml')
+      });
+      opts.includePaths.length.should.be.exactly(2);
+      opts.excludePaths.length.should.be.exactly(1);
+    });
+
+    it('should merge sclang_conf with supplied includePaths', function() {
+      var sclang = new SCLang({});
+      var opts = sclang.sclangConfigOptions({
+        sclang_conf: path.join(__dirname, 'sclang_test_conf.yaml'),
+        includePaths: [
+          '/custom/one',
+          '/path/include/one'
+        ],
+        excludePaths: [
+          '/custom/two'
+        ],
+      });
+      opts.includePaths.length.should.be.exactly(3);
+      opts.excludePaths.length.should.be.exactly(2);
+    });
+
   });
 
 });
