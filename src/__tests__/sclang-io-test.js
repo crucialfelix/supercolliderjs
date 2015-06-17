ea5b1892b0e9e97a63a87ff673bdef4d2b60da9f
crucialfelix <crucialfelix@gmail.com>
2015-06-17 18:00:03 +0200

    tests for SclangIO

---
+++ src/__tests__/sclang-io-test.js
@@ -0,0 +1,52 @@
+
+jest.autoMockOff();
+
+// imports are getting mocked
+// import {SclangIO, STATES} from '../sclang-io';
+
+var scio = require('../sclang-io');
+var SclangIO = scio.SclangIO;
+var STATES = scio.STATES;
+
+import {join} from 'path';
+import fs from 'fs';
+
+// parse a series of output files with the option to break into chunks
+function feedIt(filename, io) {
+  let abs = join(__dirname, '../../test-fixtures/', filename);
+  let contents = fs.readFileSync(abs, {encoding: 'utf8'});
+  io.parse(contents);
+}
+
+describe('sclang-io', function() {
+
+  describe('setState', function() {
+    it('should setState', function() {
+      var io = new SclangIO();
+      io.setState(STATES.BOOTING);
+      expect(io.state).toEqual(STATES.BOOTING);
+    });
+  });
+
+  it('should ignore simple text', function() {
+    var io = new SclangIO();
+    io.setState(STATES.BOOTING);
+    feedIt('io-na.txt', io);
+    expect(io.state).toEqual(STATES.BOOTING);
+  });
+
+  it('should detect succesful compile', function() {
+    var io = new SclangIO();
+    io.setState(STATES.BOOTING);
+    feedIt('io-compile-success.txt', io);
+    expect(io.state).toEqual(STATES.READY);
+  });
+
+  it('should detect programmatic compiling', function() {
+    var io = new SclangIO();
+    io.setState(STATES.READY);
+    feedIt('io-programmatic-compile.txt', io);
+    expect(io.state).toEqual(STATES.COMPILING);
+  });
+
+});
