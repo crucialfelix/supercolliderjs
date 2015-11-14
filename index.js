aea2bf31ddea0c53a745f463f15c0769e62e7d00
crucialfelix <crucialfelix@gmail.com>
2015-11-14 16:21:00 +0100

    fix index import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-exports.lang = exports.sclang = require('./lib/js/sclang');
+exports.lang = exports.sclang = require('./lib/js/lang/sclang');
 exports.server = exports.scsynth = require('./lib/js/server/server');
 exports.scapi = require('./lib/js/scapi');
-exports.resolveOptions = require('./lib/js/resolveOptions');
+exports.resolveOptions = require('./lib/js/utils/resolveOptions');
