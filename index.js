d3517c0a5273a1ce8a1a511448f2e8644a4d33cd
crucialfelix <crucialfelix@gmail.com>
2015-11-26 13:28:26 +0100

    add some common utility mapping functions. midiToFreq etc

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.lang = exports.sclang = require('./lib/lang/sclang');
 exports.server = exports.scsynth = require('./lib/server/server');
 exports.scapi = require('./lib/scapi');
 exports.resolveOptions = require('./lib/utils/resolveOptions');
+exports.map = require('./lib/map');
