1f90b75e7beaeadfbda6779e57959903b90a7659
crucialfelix <crucialfelix@gmail.com>
2015-11-30 13:03:57 +0100

    style(Logger): if running in a browser, use css color styling

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,14 +3,15 @@ var chalk = require('chalk');
 var _ = require('underscore');
 
 const c = {
-  debug: chalk.gray,
-  error: chalk.magenta,
-  stdout: chalk.green,
-  stderr: chalk.bold.red,
-  stdin: chalk.blue,
-  sendosc: chalk.cyan,
-  rcvosc: chalk.yellow
+  debug: 'gray',
+  error: 'yellow',
+  stdout: 'green',
+  stderr: 'red',
+  stdin: 'blue',
+  sendosc: 'cyan',
+  rcvosc: 'magenta'
 };
+
 
 /**
  * usage:
@@ -30,61 +31,68 @@ export default class Logger {
   constructor(debug, echo) {
     this.debug = debug;
     this.echo = echo;
+    this.browser = typeof window !== 'undefined';
   }
 
   dbug(text) {
     if (this.debug) {
-      this.print('debug   - ', text, c.debug);
+      this.print('debug  ', text, c.debug);
     }
   }
 
   err(text) {
-    this.print('error   - ', text, c.error);
+    this.print('error  ', text, c.error);
   }
 
   stdin(text) {
     if (this.echo) {
-      this.print('stdin   - ', text, c.stdin);
+      this.print('stdin  ', text, c.stdin);
     }
   }
 
   stdout(text) {
     if (this.echo) {
-      this.print('stdout  - ', text, c.stdout);
+      this.print('stdout ', text, c.stdout);
     }
   }
 
   stderr(text) {
     if (this.echo) {
-      this.print('stderr  - ', text, c.stderr);
+      this.print('stderr ', text, c.stderr);
     }
   }
 
   sendosc(text) {
     if (this.echo) {
-      this.print('sendosc - ', text, c.sendosc);
+      this.print('sendosc', text, c.sendosc);
     }
   }
 
   rcvosc(text) {
     if (this.echo) {
-      this.print('rcvosc  - ', text, c.rcvosc);
+      this.print('rcvosc ', text, c.rcvosc);
     }
   }
 
   print(label, text, color) {
-    if (typeof text !== 'string') {
-      text = JSON.stringify(text, undefined, 2);
+    if (this.browser) {
+      console.log('%c' + label, 'font-size: 10px; color:' + color, text);
+    } else {
+      // terminal
+      if (typeof text !== 'string') {
+        text = JSON.stringify(text, undefined, 2);
+      }
+      var
+        lines = text.split('\n'),
+        clean = [label + ': ' + lines[0]],
+        rest = lines.slice(1),
+        colorFn = chalk[color];
+      rest = rest.filter(function(s) { return s.length > 0; });
+      rest = rest.map(function(s) {
+        return '           ' + s;
+      });
+      clean = clean.concat(rest);
+      console.log(colorFn(clean.join('\n')));
     }
-    var
-      lines = text.split('\n'),
-      clean = [label + lines[0]],
-      rest = lines.slice(1);
-    rest = rest.filter(function(s) { return s.length > 0; });
-    rest = rest.map(function(s) {
-      return '           ' + s;
-    });
-    clean = clean.concat(rest);
-    console.log(color(clean.join('\n')));
   }
 }
