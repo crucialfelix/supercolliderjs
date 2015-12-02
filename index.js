ef4cf9df25dce3b20078959f617fc1d6cd154e89
crucialfelix <crucialfelix@gmail.com>
2015-12-02 10:55:28 +0100

    Package exports dryads
    This is temporary for this alpha branch.
    It will not be in a public release and should not be depended upon.

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ exports.lang = exports.sclang = require('./lib/lang/sclang');
 exports.server = exports.scsynth = require('./lib/server/server');
 exports.scapi = require('./lib/scapi');
 exports.resolveOptions = require('./lib/utils/resolveOptions');
+
+exports.dryads = require('./lib/server/dryads');
 exports.map = require('./lib/map');
