ca7e441b43b489f4739c6ad31304d475905d86e5
crucialfelix <crucialfelix@gmail.com>
2015-12-19 11:34:35 +0100

    unit test for crucialfelix/supercolliderjs/pull/21
    refactors server boot for easier mocking

--- src/server/server.js
+++ src/server/server.js
@@ -162,40 +162,9 @@ export class Server extends EventEmitter {
    */
   boot() {
     return new Promise((resolve, reject) => {
-      var
-        execPath = this.options.scsynth,
-        args = this.args();
-
       this.isRunning = false;
 
-      this.processEvents.onNext('Start process: ' + execPath + ' ' + args.join(' '));
-      this.process = spawn(execPath, args, {
-        cwd: this.options.cwd
-      });
-      this.processEvents.onNext('pid: ' + this.process.pid);
-
-      // when this parent process dies, kill child process
-      process.on('exit', () => {
-        if (this.process) {
-          this.process.kill('SIGTERM');
-        }
-      });
-
-      this.process.on('error', (err) => {
-        this.processEvents.onError(err);
-        this.isRunning = false;
-        // this.disconnect()
-      });
-      this.process.on('close', (code, signal) => {
-        this.processEvents.onError('Server closed. Exit code: ' + code + ' signal: ' + signal);
-        this.isRunning = false;
-        // this.disconnect()
-      });
-      this.process.on('exit', (code, signal) => {
-        this.processEvents.onError('Server exited. Exit code: ' + code + ' signal: ' + signal);
-        this.isRunning = false;
-        // this.disconnect()
-      });
+      this._spawnProcess();
 
       this._serverObservers.stdout = Observable.fromEvent(this.process.stdout, 'data', (data) => String(data));
       this._serverObservers.stdout.subscribe((e) => this.stdout.onNext(e));
@@ -225,6 +194,41 @@ export class Server extends EventEmitter {
           reject();
         }
       }, 3000);
+    });
+  }
+
+  _spawnProcess() {
+    var
+      execPath = this.options.scsynth,
+      args = this.args();
+
+    this.processEvents.onNext('Start process: ' + execPath + ' ' + args.join(' '));
+    this.process = spawn(execPath, args, {
+      cwd: this.options.cwd
+    });
+    this.processEvents.onNext('pid: ' + this.process.pid);
+
+    // when this parent process dies, kill child process
+    process.on('exit', () => {
+      if (this.process) {
+        this.process.kill('SIGTERM');
+      }
+    });
+
+    this.process.on('error', (err) => {
+      this.processEvents.onError(err);
+      this.isRunning = false;
+      // this.disconnect()
+    });
+    this.process.on('close', (code, signal) => {
+      this.processEvents.onError('Server closed. Exit code: ' + code + ' signal: ' + signal);
+      this.isRunning = false;
+      // this.disconnect()
+    });
+    this.process.on('exit', (code, signal) => {
+      this.processEvents.onError('Server exited. Exit code: ' + code + ' signal: ' + signal);
+      this.isRunning = false;
+      // this.disconnect()
     });
   }
 
