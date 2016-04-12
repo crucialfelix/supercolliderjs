1b6ff6a1854bc653eac6cb69d3e04f92248d7dd9
crucialfelix <crucialfelix@gmail.com>
2016-04-12 15:51:45 +0200

    add initial version of command middleware: scserver.sched

--- src/dryads/middleware/scserver.js
+++ src/dryads/middleware/scserver.js
@@ -3,19 +3,31 @@ import * as _  from 'underscore';
 /**
  * Send OSC to the SuperCollider server (scsynth)
  *
- * Command may have one of these three forms:
+ * Command may have one of these forms:
  *
  * msg: Array
  *
  * bundle: {
  *   time: null|Number|Array|Date
- *   packets: Array
+ *   packets: Array of OSC messages
  * }
  *
- * callAndResponse: {call: response: }
+ * callAndResponse: {
+ *   call:
+ *   response:
+ * }
+ *
+ * sched: [
+ *   {
+ *     time: null|Number|Array|Date
+ *     messages: Array of OSC messages
+ *   }
+ * ]
+ *
+ * (sched will later use a just-in-time scheduler)
  *
  * Alternatively you may supply a function that is called with context
- * and returns one of these three forms.
+ * and returns one of these forms.
  *
  * Bundle time format
  */
@@ -38,10 +50,34 @@ export default function scserver(commands) {
         let c = callIfFn(command.scserver.callAndResponse, context);
         promises.push(context.scserver.callAndResponse(c));
       }
+      if (command.scserver.sched) {
+        // {time: packets}
+        // or
+        // [{time: packets}, ]
+        let b = callIfFn(command.scserver.sched, context);
+        if (_.isArray(b)) {
+          b.forEach((bb) => context.scserver.send.bundle(bb.time, bb.packets));
+          // tried to send them all in a single osc bundle
+          // but it seems to silently choke
+          // let bundle = {
+          //   timeTag: 0.03,
+          //   packets: []
+          // };
+          // b.forEach((subBundle) => {
+          //   bundle.packets.push({
+          //     timeTag: subBundle.time,
+          //     packets: subBundle.packets
+          //   });
+          // });
+          // context.scserver.send.bundle(bundle.timeTag, bundle.packets);
+        } else {
+          context.scserver.send.bundle(b.time, b.packets);
+        }
+      }
     }
   });
   return Promise.all(promises);
-};
+}
 
 /**
  * If its a Function then call it with context
