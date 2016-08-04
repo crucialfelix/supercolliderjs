crucialfelix/supercolliderjs

commit 04d2752687ea983cbdfe775701a3942a90bd800c

Synth: if synthDef is a Dryad then invert parent-child in the subgraph
Previously it did this in one step, now there will be 2 inversions in the subgraph.

src/dryads/Synth.js
@@ -8,6 +8,9 @@ import * as _  from 'underscore';
 /**
  * Creates a synth on the server.
  *
+ * Properties:
+ * - def
+ * - args
  */
 export default class Synth extends Dryad {
 
@@ -20,6 +23,16 @@ export default class Synth extends Dryad {
   }
 
   subgraph() {
+    let def = this.properties.def;
+    if (def.isDryad) {
+      // wrap self as a child of SCSynthDef
+      let d = def.clone();
+      let m = this.clone();
+      m.properties.def = null; // will get synthDefName from context
+      d.children = [m];
+      return d;
+    }
+
     var sg = [];
     // clone and tag each one so they can be looked up during .add
     _.each(this.properties.args, (v, k) => {
@@ -31,15 +44,6 @@ export default class Synth extends Dryad {
     });
 
     sg.push(this);
-
-    // wrap self in SynthDef
-    let def = this.properties.def;
-    if (def.isDryad) {
-      let d = def.clone();
-      d.tag = 'def';
-      d.children = d.children.concat(sg);
-      return d;
-    }
 
     return new Dryad({}, sg);
   }
@@ -54,6 +58,7 @@ export default class Synth extends Dryad {
   }
 
   synthDefName(context) {
+    // The parent SCSynthDef publishes both .synthDef (object) and .synthDefName to context
     let name = _.isString(this.properties.def) ? this.properties.def : context.synthDef.name;
     if (!name) {
       throw new Error('No synthDefName supplied to Synth', context);

src/dryads/SCSynthDef.js
@@ -11,6 +11,17 @@ const StateKeys = {
 
 /**
  * Compile a SynthDef from sclang source code
+ * or load a precompiled .scsyndef
+ *
+ * If compilation is required then it will insert SCLang as a parent if necessary.
+ *
+ * properties:
+ *  - source      - sclang source code to compile
+ *  - compileFrom - path of .scd file to compile
+ *  - watch  (Boolean)     - watch compileFrom file and recompile on changes
+ *  - saveToDir   - path to save compiled .scsyndef to after compiling
+ *  - loadFrom    - path of previously compiled .scsyndef file to load to server
+ *  							This can be used to load SynthDefs without needing sclang running at all.
  *
  * `synthDef` is set in the context for children Dryads to access.
  * It is an object:
@@ -18,21 +29,12 @@ const StateKeys = {
  * - .bytes
  * - .synthDesc object with descriptive meta data
  *
- * `synthDefName` is set in context for children Dryads
+ * `synthDefName` is also set in context for children Dryads
  *
- * options:
- *  - source      - sclang source code to compile
- *  - compileFrom - path of .scd file to compile
- *  - watch       - watch compileFrom file and recompile on changes
- *  - saveToDir   - path to save compiled .scsyndef to after compiling
- *  - loadFrom    - path of .scsyndef file to load to server
+ * The synthDefName is not known until after the source code is compiled.
+ *
  */
 export default class SCSynthDef extends Dryad {
-
-  // saveToDir, watch=false,
-  // constructor(source, compileFrom, loadFrom, children=[]) {
-  //   super({source, compileFrom, loadFrom}, children);
-  // }
 
   /**
    * If there is no SCLang in the parent context,
@@ -78,6 +80,7 @@ export default class SCSynthDef extends Dryad {
   }
 
   _sendSynthDef(context, result) {
+    // ! alters context
     // name bytes
     // synthDefName should be set for child context
     this.putSynthDef(context, result.name, result.synthDesc);

commit b6461a191e9b206a38ca627ea7b9341d8aa7c041

fix: Synth .def will be undefined when SynthDef is the parent of the Synth (which happens when .def is a dryad and subgraph flips SynthDef to be parent of Synth)
bug introduced in 04d2752687ea983cbdfe775701a3942a90bd800c

src/dryads/Synth.js
@@ -24,7 +24,7 @@ export default class Synth extends Dryad {
 
   subgraph() {
     let def = this.properties.def;
-    if (def.isDryad) {
+    if (def && def.isDryad) {
       // wrap self as a child of SCSynthDef
       let d = def.clone();
       let m = this.clone();