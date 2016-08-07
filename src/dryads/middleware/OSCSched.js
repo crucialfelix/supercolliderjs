52eac1e5dca6a955815bc3c423ab76e753f2de87
crucialfelix <crucialfelix@gmail.com>
2016-08-07 12:59:57 +0200

    change(OSCSched): now takes getNextFn rather than an event list. This allows many types of iterating, looping, spawning, tempo etc functions to be implemented.
    The previous version was never released so this doesn't break any existing code.

--- src/dryads/middleware/OSCSched.js
+++ src/dryads/middleware/OSCSched.js
@@ -4,77 +4,104 @@ import { deltaTimeTag } from '../../server/osc/utils';
 /**
  * Just in time osc scheduler used by scserver middleware
  * to send OSC messages.
+ *
+ * This is used by the scserver middleware.
+ *
+ * It is used by calling `.schedLoop(getNext, epoch)`
  */
 export default class OSCSched {
 
   /**
    * constructor -
    *
-   * @param  {Function} sendFn                   function that sends OSC bundles (time, packets)
-   * @param  {integer} epoch                     timestamp of epoch that all times are relative to
-   * @param  {float} latency=0.05                just-in-time latency in seconds
-   * @param  {Function} setTimeoutFn=setTimeout  javascript setTimeout (for mocking tests)
-   * @param  {Function} clearTimeoutFn=clearTimeout javascript setInterval (for mocking tests)
-   * @return {void}
+   * @param  {Function} sendFn                   Function that sends OSC bundles to the server.
+   *                                             args: (time, msgs)
+   * @param  {number} latency=0.05               Just-in-time latency in seconds.
+   *                                             Bundles are schedule in the javascript process
+   *                                             and sent to the server just before the event time.
+   * @param  {Function} setTimeoutFn=setTimeout  JavaScript setTimeout (injectable for mocking tests)
+   * @param  {Function} clearTimeoutFn=clearTimeout JavaScript setInterval (injectable for mocking tests)
    */
-  constructor(sendFn, epoch, latency=0.05, setTimeoutFn=setTimeout, clearTimeoutFn=clearTimeout) {
+  constructor(sendFn, latency=0.05, setTimeoutFn=setTimeout, clearTimeoutFn=clearTimeout) {
     this.sendFn = sendFn;
-    this.epoch = epoch; // required
-    this.next = null;
-    this.bundles = [];
     this.latency = latency;
     this.setTimeout = setTimeoutFn;
     this.clearTimeout = clearTimeoutFn;
+
+    this.getNextFn = null;
+    this.epoch = null;
+    this.timerId = null;
   }
 
-
   /**
-   * sched - schedule OSC bundles, cancelling any scheduled from previous calls
+   * schedLoop - start a loop that gets the next event and schedules it to be sent
    *
-   * @param  {Array} bundles    Array of {time: float, packets: [oscMessages]}
-   *                            Times are in seconds relative to the epoch
-   * @return {void}
+   * @param  {Function} getNextFn A function that returns the next event object to send.
+   *
+   *                              Args: now, memo
+   *
+   *                              Returns an object:
+   *
+   *                              {time: secondsSinceEpoch, msgs: [], memo: {}}
+   *
+   *                              If it does not return anything (void) then the loop will end.
+   *
+   *                              memo is an object that the loop function can store
+   *                              state in. eg. list index for an iterator
+   *
+   *                              msgs may be an array of osc messages or a function
+   *                              called at send time that will return an array of osc messages.
+   *
+   * @param  {float} epoch     Javascript timestamp (milliseconds since 1970 UTC)
    */
-  sched(bundles) {
-    this.bundles = bundles;
+  schedLoop(getNextFn, epoch) {
+    this.getNextFn = getNextFn;
+    this.epoch = epoch;
     this._schedNext();
   }
 
-  _schedNext(startingAt=0) {
-    if (this.next) {
-      this.clearTimeout(this.next);
-      this.next = null;
+  _schedNext(memo) {
+    if (this.timerId) {
+      this.clearTimeout(this.timerId);
+      this.timerId = null;
     }
 
-    // find first bundle with positive delta
-    let now = _.now();
-    let relativeNow = now - this.epoch;
-    for (let i = startingAt; i < this.bundles.length; i += 1) {
-      let e = this.bundles[i];
-      let delta = (e.time * 1000) - relativeNow;
-      if (delta >= 0) {
-        if (delta <= (this.latency * 1000)) {
-          // send immediately and step
-          this._send(e);
-          this._schedNext(i + 1);
-        } else {
-          this._jitToNext(delta - this.latency, e, i);
-        }
-
-        return;
+    const now = (_.now() - this.epoch) / 1000;
+    const event = memo ? this.getNextFn(now, memo) : this.getNextFn(now);
+    if (event) {
+      const delta = event.time - now;
+      if (delta <= this.latency) {
+        this._send(event);
+        this._schedNext(event.memo);
+      } else {
+        this._jitSend(delta, event);
       }
     }
   }
 
-  _jitToNext(delta, event, i) {
-    this.next = this.setTimeout(() => {
-      this.next = null;
+  /**
+   * _jitSend - schedule to send the event just before it should play on the server.
+   *
+   * Cancels any previously scheduled event.
+   *
+   * @param  {float} delta seconds to wait
+   * @param  {Object} event With .msgs .time and optional .memo
+   *                        to be passed to the next call to getNextFn
+   */
+  _jitSend(delta, event) {
+    this.timerId = this.setTimeout(() => {
+      this.timerId = null;
       this._send(event);
-      this._schedNext(i + 1);
-    }, delta);
+      this._schedNext(event.memo);
+    }, delta * 1000);
   }
 
+  /**
+   * _send - send the OSC bundle
+   *
+   * @param  {Object} event description
+   */
   _send(event) {
-    this.sendFn(deltaTimeTag(event.time, this.epoch), event.packets);
+    this.sendFn(deltaTimeTag(event.time * 1000, this.epoch), event.msgs);
   }
 }
