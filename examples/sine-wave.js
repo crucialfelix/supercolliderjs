78481d7711ff3de2766744c6d9331fb8ccec88e9
Chris Sattinger <crucialfelix@gmail.com>
2019-12-06 19:58:00 +0100

    Simplify sine-wave example

--- examples/sine-wave.js
+++ examples/sine-wave.js
@@ -1,23 +1,17 @@
-const d = require("supercolliderjs").dryads;
+const { SCLang, SCServer, Synth, dryadic } = require("supercolliderjs").dryads;
 
 /**
- * This is the full tree required just to play a Sin wave.
- * Dryadic 2 will
+ * Minimal Sin wave example.
+ *
+ * To compile the SuperCollider language synth def it requires a
+ * SCLang. To play the Synth it requires a SCServer.
  */
-const out = new d.SCLang({}, [
-  new d.SCServer({ numInputBusChannels: 0 }, [
-    new d.Synth({
-      def: new d.SCSynthDef({
-        source: `
-      { arg freq;
-        Out.ar(0, SinOsc.ar(freq))
-      }`,
-      }),
-      args: {
-        freq: 440,
-      },
+const out = new SCLang({}, [
+  new SCServer({}, [
+    Synth.fromSource(`|freq| SinOsc.ar(freq)`, {
+      freq: 440,
     }),
   ]),
 ]);
 
-d.dryadic(out).play();
+dryadic(out).play();
