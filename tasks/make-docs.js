26eba17e88f31e907eb073194e715d2fd35e8c81
Chris Sattinger <crucialfelix@gmail.com>
2019-12-08 14:53:15 +0100

    Render my own typedoc formatting for APIs to markdown/html

--- tasks/make-docs.js
+++ tasks/make-docs.js
@@ -3,19 +3,26 @@ const Mustache = require("mustache");
 const path = require("path");
 const fs = require("fs");
 const fsp = fs.promises;
+const { renderDocForName } = require("./render-api");
 
 const root = path.resolve(path.join(__dirname, ".."));
 const packagesRoot = path.join(root, "packages");
+const docsSrc = path.join(root, "docs", "src", "packages");
+
 // const docsRoot = path.join(root, "docs");
 const repository = "https://github.com/crucialfelix/supercolliderjs";
 
 const typedocRoot = "https://crucialfelix.github.io/supercolliderjs";
 
 // Test locally with:
-// const typedocRoot = "http://localhost:3000";
+const docsRoot = "http://localhost:3000/#";
 
+const packageNames = async () => (await fsp.readdir(packagesRoot)).filter(name => name.match(/^[a-z\-]+$/));
 async function fileExists(filePath) {
-  return fsp.stat(filePath).then(stat => true, err => false);
+  return fsp.stat(filePath).then(
+    stat => true,
+    err => false,
+  );
 }
 
 const triple = "```";
@@ -36,6 +43,10 @@ const typedocLink = text => {
   const body = `[${title}](${typedocRoot}${link})`;
   return body;
 };
+const renderApi = text => {
+  const [package, name] = text.split(":", 2);
+  return renderDocForName(package, name);
+};
 
 async function make(srcPath, destPath, data) {
   const tplPath = path.join(root, srcPath);
@@ -48,8 +59,56 @@ async function make(srcPath, destPath, data) {
   await fsp.writeFile(path.join(root, destPath), content);
 }
 
+async function parseSidebar() {
+  // auto generate md files if they are listed in the sidebar
+  const sidebar = await fsp.readFile(path.join(root, "docs", "_sidebar.md"), { encoding: "utf8" });
+  const autos = {};
+  function pushAuto(package, title, filename) {
+    if (!autos[package]) {
+      autos[package] = [];
+    }
+    autos[package].push({ title, filename });
+  }
+  const re = /\[(.+)\]\(packages\/([a-z\-_]+)\/([^)]+)\)/;
+  for (const line of sidebar.split("\n")) {
+    // if has []()
+    // if file is not README
+    //
+    const match = line.match(re);
+    if (match) {
+      const title = match[1];
+      const package = match[2];
+      const filename = match[3];
+      if (filename !== "README.md") {
+        pushAuto(package, title, filename);
+      }
+    }
+  }
+  return autos;
+}
+
+function generateAutodocument(package, { title, filename }) {
+  const BBL = "{{";
+  const BBR = "}}";
+  const BBBL = "{{{";
+  const BBBR = "}}}";
+
+  const p = path.join(docsSrc, package, filename);
+  const fullname = filename.replace("_", "/").replace(".md", "");
+  console.log({ filename, fullname, title, package });
+
+  const body = `# ${BBBL}name${BBBR} ${title}
+
+${BBL}#api${BBR}${package}:${fullname}${BBL}/api${BBR}
+`;
+
+  console.log({ body, p });
+
+  fs.writeFileSync(p, body, { encoding: "utf-8" });
+}
+
 async function main(version) {
-  const packages = (await fsp.readdir(packagesRoot)).filter(name => name.match(/^[a-z\-]+$/));
+  const packages = await packageNames();
 
   const pkg = JSON.parse(await fsp.readFile(path.join(root, "package.json")));
 
@@ -60,38 +119,54 @@ async function main(version) {
     version: version || pkg.version,
     homepage: pkg.hompage,
     repository,
-    typedocRoot: typedocRoot,
+    docsRoot,
+    typedocRoot,
     example: () => example,
     typedocLink: () => typedocLink,
+    api: () => renderApi,
   };
 
   // _coverpage
   await make("docs/src/_coverpage.md", "docs/_coverpage.md", rootData);
 
+  const autos = await parseSidebar();
+
   for (const pkgdir of packages) {
     const pkg = JSON.parse(await fsp.readFile(path.join(packagesRoot, pkgdir, "package.json")));
-
+    const short = pkg.name.replace("@supercollider/", "");
     const data = {
       ...rootData,
       name: pkg.name,
-      short: pkg.name.replace("@supercollider/", ""),
+      short,
       description: pkg.description,
       version: pkg.version,
       homepage: pkg.hompage,
     };
 
-    const tplPath = path.join("docs/src/packages", pkgdir, "README.md");
+    // generate autodocument for each entry in sidebar
+    for (const sidebarLink of autos[short] || []) {
+      generateAutodocument(short, sidebarLink);
+    }
 
-    // Write to packages for publishing to npm
-    await make(tplPath, path.join("packages", pkgdir, "README.md"), data);
-    // Write it to docs
-    await make(tplPath, path.join("docs/packages", pkgdir, "README.md"), data);
+    for (const filename of fs.readdirSync(path.join("docs/src/packages", pkgdir))) {
+      if (filename.endsWith(".md")) {
+        const tplPath = path.join("docs/src/packages", pkgdir, filename);
 
-    if (pkgdir === "supercolliderjs") {
-      // Is also the main page in docs
-      await make(tplPath, path.join("docs", "README.md"), data);
-      // and main page on github
-      await make(tplPath, "README.md", data);
+        // Write it to docs
+        await make(tplPath, path.join("docs/packages", pkgdir, filename), data);
+
+        if (filename === "README.md") {
+          // Write to packages for publishing to npm
+          await make(tplPath, path.join("packages", pkgdir, filename), data);
+
+          if (pkgdir === "supercolliderjs") {
+            // Is also the main page in docs
+            await make(tplPath, path.join("docs", "README.md"), data);
+            // and main page on github
+            await make(tplPath, "README.md", data);
+          }
+        }
+      }
     }
   }
 }
---
+++ tasks/render-api.js
@@ -0,0 +1,308 @@
+const path = require("path");
+const fs = require("fs");
+
+const root = path.resolve(path.join(__dirname, ".."));
+const packagesRoot = path.join(root, "docs", "src", "packages");
+
+const apis = {};
+
+function loadApi(package) {
+  if (!apis[package]) {
+    apis[package] = JSON.parse(fs.readFileSync(path.join(packagesRoot, package, "api.json")));
+  }
+  return apis[package];
+}
+
+function find(node, name) {
+  // ignore a property with that name: it is perhaps an export
+  // or just a parent class that holds the thing we are searching for.
+  if (node.name === name && node.kindString !== "Property") {
+    return node;
+  }
+  // console.log(node);
+  if (!node.children) return;
+  for (const child of node.children) {
+    const cn = find(child, name);
+    if (cn) {
+      return cn;
+    }
+  }
+}
+
+const json = obj => JSON.stringify(obj, null, 2);
+
+const joinWith = (lines, joiner = "\n\n") => lines.filter(l => !!l).join(joiner);
+const join = (...lines) => joinWith(lines, "");
+const joinc = lines => joinWith(lines, ", ");
+const joins = (...lines) => joinWith(lines, " ");
+const joinnl = lines => joinWith(lines, "\n");
+
+const el = (element, body, attrs = "") => `<${element} ${attrs}>${body}</${element}>`;
+
+// const el = (element, attrs = {}) => body => `<${element} ${attrs}>${body}</${element}>`;
+const h4 = (txt, id = "") => `<h4 id="ref-${id}">${txt}</h4>`;
+const span = (txt, className = "") => `<span class="${className}">${txt}</span>`;
+const div = (txt, className = "") => `<div class="${className}">${txt}</div>`;
+const ul = (lis, className = "no-dot") =>
+  `<ul class="${className}">` + join(...lis.map(li => `<li>${li}</li>`)) + "</ul>";
+
+const shortText = txt => txt && el("p", txt);
+const text = txt => txt && el("p", txt); // It's markdown, needs to be processed
+const returns = txt => txt && `Returns ${txt}`;
+
+/**
+ * Render a type definition
+ */
+const type = obj => {
+  const tc = (name, className = "") => span(name, `type ${className}`);
+
+  if (!obj) return "";
+  switch (obj.type) {
+    case "intrinsic":
+      // function token function
+      // token boolean string
+      return tc(obj.name, "token entity");
+    case "reference": {
+      // can link to it if it has an id
+      // or link to other package if known
+      let name = obj.name;
+      if (obj.typeArguments) {
+        name += `&lt;${joinc(obj.typeArguments.map(type))}&gt;`;
+      }
+
+      const span = tc(name, obj.type);
+      // hard to link to a ref on this page without knowing
+      // the base url
+      // if (obj.id) {
+      //   return `<a href="?id=ref-${obj.id}">${span}</a>`;
+      // }
+      // may be able to link to the other package
+      return span;
+    }
+    case "array":
+      return tc(obj.elementType) + "[]";
+    case "union":
+      return tc(obj.types.map(type).join(" | "));
+    case "tuple":
+      return `[${tc(obj.elements.map(type).join(", "))}]`;
+    case "reflection": {
+      // may have a reference inside this
+      // indexSignature
+      // signatures
+      const sigs = obj.declaration.indexSignature || obj.declaration.signatures;
+      return sigs ? type(sigs[0].type) : "reflection";
+    }
+    case "intersection": {
+      return obj.types.map(type).join(" &amp; ");
+    }
+    default:
+      console.error(obj);
+
+      throw new Error("Render type: unknown type:" + obj.type);
+  }
+};
+
+// TODO
+// sources
+// flags
+const private = name => `<!-- private ${name} -->`;
+
+const comment = c => {
+  return c && join(shortText(c.shortText), text(c.text), returns(c.returns));
+};
+
+const Class = node => {
+  const xs = node.extendedTypes ? joinc(node.extendedTypes.map(type)) : null;
+
+  return div(
+    join(
+      `<h3 class="class-header" id="${node.name}">class <span class="class">${node.name}</span></h3>`,
+      xs && `extends: ${xs}`,
+      comment(node.comment),
+      ...node.children.map(renderNode),
+    ),
+    "Class",
+  );
+};
+
+const Property = node => {
+  if (node.flags && node.flags.isPrivate) {
+    return private(node.name);
+  }
+  if (node.inheritedFrom) {
+    // or show mini with link
+    return null;
+  }
+
+  // flags: static
+  const header = h4(span(node.name, "token property") + " " + type(node.type), node.id);
+  return joinnl([header, comment(node.comment)]);
+};
+
+const Accessor = node => {
+  if (node.flags && node.flags.isPrivate) {
+    return private(node.name);
+  }
+  const ty = node.getSignature[0].type;
+  return join(h4(span(node.name, "token property") + " " + type(ty), node.id), comment(node.comment));
+};
+
+const Method = node => {
+  if (node.flags && node.flags.isPrivate) {
+    return private(node.name);
+  }
+  if (node.inheritedFrom) {
+    // or show mini with link
+    return null;
+  }
+  const signature = node.signatures[0];
+  return join(h4(functionTitle(signature), node.id), comment(signature.comment));
+};
+
+const Constructor = node => {
+  return Method(node);
+};
+
+const Parameter = parameter => {
+  const p = `${parameter.name}: ${type(parameter.type)}`;
+  if (parameter.defaultValue) {
+    return `${p} = ${parameter.defaultValue}`;
+  }
+  return p;
+};
+
+const functionParameters = parameters => {
+  return `(${joinWith((parameters || []).map(Parameter), ", ")})`;
+};
+const functionTitle = signature => {
+  return `${span(signature.name, "token function")}${functionParameters(signature.parameters)}: ${type(
+    signature.type,
+  )}`;
+};
+// bigger with all params and comments
+const CallSignature = signature => {
+  return join(functionTitle(signature), comment(signature.comment));
+  // TODO flags
+  // TODO breakout parameters that have comments and types
+};
+
+const pre = obj => `<pre>${JSON.stringify(obj, null, 2)}</pre>\n`;
+
+const nodeSummary = node => `${node.kindString} ${node.name}`;
+// pre({ name: node.name, kindString: node.kindString, comment: comment(node.comment) });
+// const nodeSummary = node => pre({ name: node.name, kindString: node.kindString, comment: comment(node.comment) });
+
+const ExternalModule = node => {
+  if (node.children) {
+    return div(
+      join(
+        `${span("module", "token keyword")} ${node.name}`,
+        ...node.children
+          .map(node.children.length > 100 ? nodeSummary : renderNode)
+          .map(html => div(html, "module-child")),
+      ),
+      "module",
+    );
+  }
+  return div(node.name, "Module");
+  // return `Empty module ${JSON.stringify(node)}`;
+};
+
+const indexEntry = node => {
+  if (node.kindString === "External module") {
+    return ExternalModule(node);
+  }
+  return `${node.kindString} ${node.name}`;
+};
+
+const Index = node => {
+  return div(join(node.name, ul(node.children.map(indexEntry))), "Index");
+};
+
+const Interface = node => {
+  const children = (node.children || []).map(renderNode);
+  return div(join(h4(joins(span("interface", "token keyword"), node.name), node.id), ul(children)), "Interface");
+};
+
+const TypeAlias = node => {
+  // name
+  // type
+  const txt = `${span("type", "token keyword")} ${node.name} = ${type(node.type)};`;
+  return div(txt, "TypeAlias");
+};
+
+const Enumeration = node => {
+  return div(
+    join(
+      h4(joins(span("enum", "token keyword"), node.name), node.id),
+      ul(node.children.map(child => joins(child.name, "=", child.defaultValue))),
+      comment(node.comment),
+    ),
+    "Enum",
+  );
+};
+
+const ObjectLiteral = node => {
+  return join(h4(joins(span(node.name, "token variable"), "= {")), ...ul(node.children.map(renderNode)), "}");
+};
+
+const Variable = node => {
+  return joins(span(node.name, "token property"), ":", type(node.type), "=", node.defaultValue);
+};
+
+const Function = Method;
+
+// [node.kindString] : function
+const renderers = {
+  default: pre,
+  Class,
+  Constructor,
+  Property,
+  Variable,
+  Accessor,
+  Method,
+  Function,
+  Parameter,
+  Interface,
+  Enumeration,
+  "Object literal": ObjectLiteral,
+  "Type alias": TypeAlias,
+  "Call signature": CallSignature,
+  "Constructor signature": CallSignature,
+  "External module": ExternalModule,
+  // kind: 0 is the top root of typedocs's api.json
+  0: Index,
+};
+
+function renderNode(node) {
+  const fn = renderers[node.kindString || node.kind] || renderers.default;
+  return (fn(node) || "").trim();
+}
+
+function renderDocForName(package, name) {
+  console.log("renderDocForName", { package, name });
+
+  const api = loadApi(package);
+  let node = find(api, name);
+  if (!node) {
+    if (!name.startsWith(`"`)) {
+      node = find(api, `"${name}"`);
+    }
+  }
+  if (!node) {
+    console.error(`ERROR: Unable to find an entry for ${name} in ${package} api.json`);
+    // console.log(xxddapi);
+    console.log(JSON.stringify(namesInApi(api), null, 2));
+    // throw new Error();
+    return "";
+  }
+  return renderNode(node);
+}
+
+const namesInApi = api => ({
+  [api.name]: api.children ? api.children.map(namesInApi) : null,
+});
+
+module.exports = {
+  renderDocForName,
+};
